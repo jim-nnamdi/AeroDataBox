01613178d8f47b4fa37d942d30aed923a1bcd91d
jim-nnamdi <jimsamuel50@gmail.com>
2022-10-03 16:54:31 +0100

    initial endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+// use serde::{Serialize, Deserialize};
+
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    const BASEURL: &str = "https://aerodatabox.p.rapidapi.com";
+    let borrowed_query: &str ="/flights/%7BsearchBy%7D/KL1395/2020-06-10";
+
+    let client = reqwest::Client::new();
+    let aeros = client.get(format!("{}{}", BASEURL, borrowed_query))
+    .header("X-RapidAPI-Key", "53fd0041f2msh8c3ffa5b5508be0p152202jsn9a0f742df4a8")
+    .header("X-RapidAPI-Host", "aerodatabox.p.rapidapi.com")
+    .send()
+    .await?
+    .text()
+    .await?;
+
+    println!("{:#?}", aeros);
+
+    Ok(())
 }
